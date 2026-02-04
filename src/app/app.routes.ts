cd3bfd563f7ee27c1ac78ecbb24b0962fbca7078
Joseph Montoya <j.jhoel@outlook.es>
2026-02-04 00:13:07 -0500

    Agregando rutas de navegacion para login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './auth/layout/auth-layout/auth-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: ()=>import('./auth/auth.routes'),
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
