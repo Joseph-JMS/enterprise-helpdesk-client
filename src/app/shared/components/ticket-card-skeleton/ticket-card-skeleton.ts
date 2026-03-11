3586964b40cdab6aad9287f9920cf47d32455be6
Joseph Montoya <j.jhoel@outlook.es>
2026-03-11 16:53:00 -0500

    implementando card-skeleton de carga

---
+++ src/app/shared/components/ticket-card-skeleton/ticket-card-skeleton.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ticket-card-skeleton',
+  imports: [],
+  templateUrl: './ticket-card-skeleton.html',
+})
+export class TicketCardSkeleton { }
