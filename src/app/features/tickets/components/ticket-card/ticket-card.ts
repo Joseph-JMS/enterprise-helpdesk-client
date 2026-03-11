7a727ae82a616995dfec039f180d9f35beb43783
Joseph Montoya <j.jhoel@outlook.es>
2026-03-11 16:23:58 -0500

    Impl ticket card

--- src/app/features/tickets/components/ticket-card/ticket-card.ts
+++ src/app/features/tickets/components/ticket-card/ticket-card.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { TicketResponse } from '../../../../core/interfaces/ticket.interface';
+import { StatusLabelPipe } from '../../../../core/pipes/status-label.pipe';
+import { StatusClassPipe } from '../../../../core/pipes/status-class.pipe';
+import { PriorityLabelPipe } from '../../../../core/pipes/priority-label.pipe';
+import { PriorityClassPipe } from '../../../../core/pipes/priority-class.pipe';
+import { DatePipe } from '@angular/common';
+import { TruncatePipe } from '../../../../core/pipes/truncate.pipe';
 
 @Component({
   selector: 'ticket-card',
-  imports: [RouterLink],
+  imports: [StatusLabelPipe, StatusClassPipe, PriorityLabelPipe, PriorityClassPipe, DatePipe, TruncatePipe],
   templateUrl: './ticket-card.html',
 })
-export class TicketCard { }
+export class TicketCard {
+
+  ticket = input.required<TicketResponse>();
+
+  onView = output<number>();
+  onCancel = output<number>();
+  onAssign = output<number>();
+  onChangeStatus = output<number>();
+}
