5ce6e2f294f198167a9d537967e3408c68187b63
Joseph Montoya <j.jhoel@outlook.es>
2026-06-30 17:53:53 -0500

    feat: add category report

--- src/app/features/reports/pages/category-report/category-report.ts
+++ src/app/features/reports/pages/category-report/category-report.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { CategoryReportResponse } from '../../../../core/interfaces/report.interface';
+import { ReportService } from '../../../../core/services/report.service';
 
 @Component({
   selector: 'app-category-report',
   imports: [],
   templateUrl: './category-report.html',
 })
-export class CategoryReport { }
+export class CategoryReport {
+  private readonly reportService = inject(ReportService);
+
+  categories = signal<CategoryReportResponse[]>([]);
+  isLoading = signal<boolean>(true);
+  errorMessage = signal<string | null>(null);
+
+  maxTickets = computed(() =>
+  Math.max(...this.categories().map(c => c.totalTickets), 1)
+);
+
+  ngOnInit() {
+    this.reportService.getByCategory().subscribe({
+      next: (data) => {
+        this.categories.set(data);
+        this.isLoading.set(false);
+      },
+      error: () => {
+        this.errorMessage.set('Error al cargar el reporte');
+        this.isLoading.set(false);
+      }
+    });
+  }
+}
