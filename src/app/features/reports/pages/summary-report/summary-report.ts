a53040bc0b2daf56f152db598fbcc2a8813a3fde
Joseph Montoya <j.jhoel@outlook.es>
2026-06-30 17:54:58 -0500

    feat: add summary report

--- src/app/features/reports/pages/summary-report/summary-report.ts
+++ src/app/features/reports/pages/summary-report/summary-report.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ReportService } from '../../../../core/services/report.service';
+import { SummaryResponse } from '../../../../core/interfaces/report.interface';
 
 @Component({
   selector: 'app-summary-report',
   imports: [],
   templateUrl: './summary-report.html',
 })
-export class SummaryReport { }
+export class SummaryReport {
+  private readonly reportService = inject(ReportService);
+
+  summary = signal<SummaryResponse | null>(null);
+  isLoading = signal<boolean>(true);
+  errorMessage = signal<string | null>(null);
+
+  ngOnInit() {
+    this.reportService.getSummary().subscribe({
+      next: (data) => {
+        this.summary.set(data);
+        this.isLoading.set(false);
+      },
+      error: () => {
+        this.errorMessage.set('Error al cargar el resumen');
+        this.isLoading.set(false);
+      }
+    });
+  }
+}
