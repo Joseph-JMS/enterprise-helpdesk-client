98801174a0adefb3edf427c7a89247c8eac81b53
Joseph Montoya <j.jhoel@outlook.es>
2026-06-30 17:54:26 -0500

    feat: add sla report

--- src/app/features/reports/pages/sla-report/sla-report.ts
+++ src/app/features/reports/pages/sla-report/sla-report.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ReportService } from '../../../../core/services/report.service';
+import { AuthService } from '../../../../core/services/auth.service';
+import { SlaReportResponse } from '../../../../core/interfaces/report.interface';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-sla-report',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './sla-report.html',
 })
-export class SlaReport { }
+export class SlaReport {
+  private readonly reportService = inject(ReportService);
+  private readonly authService = inject(AuthService);
+
+  readonly isAdmin = this.authService.isAdmin;
+
+  slaData = signal<SlaReportResponse[]>([]);
+  isLoading = signal<boolean>(true);
+  errorMessage = signal<string | null>(null);
+
+  ngOnInit() {
+    const request$ = this.isAdmin()
+      ? this.reportService.getSla()
+      : this.reportService.getMySla();
+
+    request$.subscribe({
+      next: (data) => {
+        this.slaData.set(data);
+        this.isLoading.set(false);
+      },
+      error: () => {
+        this.errorMessage.set('Error al cargar el reporte SLA');
+        this.isLoading.set(false);
+      }
+    });
+  }
+}
